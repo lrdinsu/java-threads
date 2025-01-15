7ccc89c3fabe784ec2a5a3cc3f88b04a2ca33605
lilia <143701282+lrdinsu@users.noreply.github.com>
2025-01-15 09:31:25 -0800

    implement tests for java memory model

---
+++ src/memory/MyRunnable.java
@@ -0,0 +1,19 @@
+package memory;
+
+public class MyRunnable implements Runnable{
+
+    private int count = 0;
+
+    @Override
+    public void run() {
+
+        for(int i=0; i<1_000_000; i++) {
+            synchronized (this) {
+                this.count++;
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " : " + this.count);
+    }
+
+}
+
---
+++ src/memory/SharedObject.java
@@ -0,0 +1,15 @@
+package memory;
+
+public class SharedObject {
+
+    public static void main(String[] args) {
+
+        Runnable runnable = new MyRunnable();
+
+        Thread thread1 = new Thread(runnable, "Thread 1");
+        Thread thread2 = new Thread(runnable, "Thread 2");
+
+        thread1.start();
+        thread2.start();
+    }
+}
